156ae058dc1719987e891ab753a2aec0a82ba327
Henriquefvieira <hferreira22@gmail.com>
2023-09-05 17:44:13 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-# Alterar nomes das variaveis para ingles
-# Não utilizar numeros nas nomeações das variaveis (pesquisar sobre operadores e operandos na calculadora)
-# Tratamento para caso de inserção de letras nas variaveis num1 e num2
-# PEP8
+def calculator(operation, primary_value, secondary_value):
+    if operation =='+':
+        return primary_value + secondary_value
+    elif operation =='-':
+        return primary_value - secondary_value
+    elif operation =='*':
+        return primary_value * secondary_value
+    elif operation =='/':
+        if secondary_value != 0:
+            return primary_value / secondary_value
+        else:
+            return "Divisão por zero não é permitida"
+
+    else:
+        return "Operação inválida"
 
 while True:
-    operacao = input('Qual operação (+,-,*,/) você quer fazer? Ou \'q\' para sair ')
-    if operacao == 'q':
+    operation = input('Qual operação (+,-,*,/,%,raiz) você quer fazer ou \'q\' para sair ')
+    if operation == 'q':
         break
-    elif operacao == '+' or operacao == '-' or operacao == '*' or operacao == '/':
-        num1 = int(input('Digite o primeiro numero: '))
-        num2 = int(input('Digite o segundo numero: '))
-
-    if operacao == '+':
-        total = num1 + num2
-        print(total)
-    elif operacao == '-':
-        total = num1 - num2
-        print(total)
-    elif operacao == '*':
-        total = num1 * num2
-        print(total)
-    elif operacao == '/':
-        total = num1 / num2
-        print(total)
+    elif operation in ('+', '-', '*', '/'):
+        try:
+            primary_value = float(input('Digite o primeiro número: '))
+            secondary_value = float(input('Digite o segundo número: '))
+        except ValueError:
+            print('Por favor, insira apenas números válidos.')
+            continue
+        result = calculator(operation, primary_value, secondary_value)
+        print (f'Resultado: {result}')
+    elif operation == 'raiz':
+        import math
+        num = float(input('Qual número desaja saber a raiz quadrada?'))
+        raiz = math.sqrt(num)
+        print(f'A raiz quadrada de {num} é {raiz}')
+    elif operation =='%':
+        value = float(input('Qual valor deseja saber a porcentagem:'))
+        porcent = float(input('Quantos porcento do valor deseja saber: '))/100
+        value_finish = porcent * value
+        print(f'{porcent} de {value} é: {value_finish}')
     else:
-        print('Operação é invalida')
+        print('Por favor, insira apenas operações válidas')
