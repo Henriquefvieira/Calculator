014ca8761dffce9286202171fc4d1b4d2e132721
Henriquefvieira <hferreira22@gmail.com>
2023-09-11 11:52:11 -0300

    Correção

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import math
 
 def calculator(operation, primary_value, secondary_value):
-    try:
-        if operation =='+':
-            return primary_value + secondary_value
-        elif operation =='-':
-            return primary_value - secondary_value
-        elif operation =='*':
-            return primary_value * secondary_value
-        elif operation =='/':
-            try:
-                return primary_value / secondary_value
-            except ZeroDivisionError:
-                return '\033[31;1mDivisão por zero não é permitida\033[0;0m'
-        elif operation == 'raiz':
-            return math.sqrt(primary_value)
-        elif operation == '%':
-            return (secondary_value / 100) * primary_value
-        else:
-            return "\033[31;1mOperação inválida\033[0;0m"
-    except (ValueError, ZeroDivisionError) as e:
-        return str(e)
+    if operation =='+':
+        return primary_value + secondary_value
+    elif operation =='-':
+        return primary_value - secondary_value
+    elif operation =='*':
+        return primary_value * secondary_value
+    elif operation =='/':
+        try:
+            return primary_value / secondary_value
+        except ZeroDivisionError:
+            return '\033[31;1mDivisão por zero não é permitida\033[0;0m'
+    elif operation == 'raiz':
+        return math.sqrt(primary_value)
+    else:
+        return "\033[31;1mOperação inválida\033[0;0m"
 
 while True:
     operation = input('Qual operação \033[34;1m(+,-,*,/,%,raiz)\033[0;0m você quer fazer ou \'q\' para sair: ')
@@ -39,5 +34,10 @@ while True:
         primary_value = float(input('Qual número desaja saber a raiz quadrada?'))
         result = calculator(operation, primary_value, None)
         print(f'A raiz quadrada de {primary_value} é {result}')
+    elif operation =='%':
+        primary_value = float(input('Qual valor deseja saber a porcentagem: '))
+        secondary_value = float(input('Quantos porcento do valor deseja saber: '))/100
+        value_finish = secondary_value * primary_value
+        print(f'{secondary_value} de {primary_value} é: {value_finish}')
     else:
         print('\033[31;1mPor favor, insira apenas operações válidas\033[0;0m')
